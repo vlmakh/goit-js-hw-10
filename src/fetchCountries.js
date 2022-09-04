c23989b633d7e042df66d15d42edb48d6e8e3816
vlmakh <vlmakh@gmail.com>
2022-09-04 10:32:40 +0300

    add fetch function to separate file

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,15 @@
+export function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.com/v3.1/name/${searchQuery}`).then(
+    response => {
+      return response.json();
+    }
+  );
+}
 
+// `https://restcountries.com/v3.1/name/${searchQuery}?fields=name,capital,currencies`
+
+// https://restcountries.com/v2/{service}?fields={field},{field},{field}
+// https://restcountries.com/v2/all?fields=name,capital,currencies
+
+// ограничить поиск по определенным полям
+// вывести языки из объекта
--- src/index.js
+++ src/index.js
@@ -1,60 +1,59 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
 const inputRef = document.querySelector('#search-box');
 const divInfoRef = document.querySelector('.country-info');
 const ulListRef = document.querySelector('.country-list');
 
-inputRef.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+inputRef.addEventListener('input', debounce(onInputType, DEBOUNCE_DELAY));
 
-const country = inputRef.value;
-
-function onInputType(country) {
-  //   fetchCountries(country)
-  //   if ((countries.length = 1)) {
-  //     console.log('ОДНА');
-  //     // ulListRef.innerHTML = '';
-  //     // divInfoRef.innerHTML = markupCard(countries[0]);
-  //   } else if (countries.length >= 2 && countries.length <= 10) {
-  //     // divInfoRef.innerHTML = '';
-  //     console.log('от 2 до 10');
-  //   }
-  //   // divInfoRef.innerHTML = markupCard(countries[0]);
-  //   else {
-  //     // ulListRef.innerHTML = '';
-  //     // divInfoRef.innerHTML = '';
-  //     console.log('БОЛЬШЕ 10');
-  //   }
+function onInputType() {
+  fetchCountries(inputRef.value)
+    .then(markup)
+    .catch(error => Notiflix.Notify.failure(`${error}`));
 }
 
-function fetchCountries() {
-  fetch(`https://restcountries.com/v3.1/name/${inputRef.value}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(countries => {
-      console.log(countries.length);
+function markup(countries) {
+  clearPage();
 
-      countries.forEach(markupList);
-    })
-    .catch(error => console.log(error));
+  console.log(countries[0]);
+
+  if (countries.length === 1) {
+    markupCard(countries[0]);
+  } else if (countries.length >= 2 && countries.length <= 10) {
+    countries.forEach(markupList);
+  } else if (countries.length > 10) {
+    clearPage();
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
 }
 
 function markupCard(country) {
-  return `<div class="country-label">
+  ulListRef.innerHTML = '';
+  divInfoRef.innerHTML = `<div class="country-label">
         <img src="${country.flags.svg}" alt="${country.name.common}" class="country-flag" />
-        <h1 class="country-name">${country.name.official}</h1>
+        <h1 class="country-name">${country.name.common}</h1>
       </div>
       <p class="paragraph">Capital: <span class="value">${country.capital}</span></p>
       <p class="paragraph">Population: <span class="value">${country.population}</span></p>
-      <p class="paragraph">Languages: <span class="value">${country.fifa}</span></p>
+      <p class="paragraph">Languages: <span class="value">${country.languages}</span></p>
       `;
 }
 
+function chooseLanguages(country) {
+  console.dir(country[0].languages);
+}
+
 function markupList(country) {
+  divInfoRef.innerHTML = '';
   ulListRef.insertAdjacentHTML(
     'beforeend',
     `<li>
@@ -62,7 +61,12 @@ function markupList(country) {
           src="${country.flags.svg}"
           alt="${country.name.common}"
           class="country-flag"
-        />${country.name.official}
+        />${country.name.common}
       </li>`
   );
+}
+
+function clearPage() {
+  ulListRef.innerHTML = '';
+  divInfoRef.innerHTML = '';
 }
